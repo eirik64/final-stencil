6759bd3c4dbd4e0f71c190b6444b847d54c2d5c1
Ashley Chung <ashleychung@Ashleys-MacBook-Pro-5.local>
2020-12-02 16:19:39 -0500

    Added Generate String Method for L-System

--- src/TreeScene.cpp
+++ src/TreeScene.cpp
@@ -1,4 +1,6 @@
 #include "TreeScene.h"
+#include <iostream>
+#include <string>
 
 // Default constructor
 TreeScene::TreeScene() {}
@@ -14,5 +16,17 @@ TreeScene::~TreeScene()
  *  Input parameters: width (int), height(int), and number of trees in the scene.
  *  Width and heights will essentially be width and height of the cylinder
  *  forming the main bark of the tree.
+ *  Function: parses the string generated from the l-system rules
+ *  and generates the tree.
  */
 void TreeScene::setupTree(int width, int height, int numTree) {}
+
+std::string TreeScene::generateString() {
+    std::string initialSentence = axiom; // starting with axiom.
+    std::string resultString;
+    for (int i=0; i < initialSentence.length(); ++i) {
+     //   std::string currentWord = initialSentence[i]
+        // still working on this (splitting the character, and then creating a new string based on each character
+        // will update this method. (Ashley)
+    }
+}
--- src/TreeScene.h
+++ src/TreeScene.h
@@ -1,17 +1,29 @@
 #ifndef TREESCENE_H
 #define TREESCENE_H
+#include <iostream>
+#include <string>
 
+// rule 2
+struct rule {
+    // variables: A, B, +, -, [, ]
+    // axiom: A
+    // rule: A → A+-[B+A-[B]]-[AB]
+    std::string startingVar = "A";
+    std::string convertedVar = "A+-[B+A-[B]]-[AB]"; // will update this. (Ashley)
+};
 
 class TreeScene {
     public: // public methods, global variables, constants here
         TreeScene(); // constructor
         ~TreeScene(); // destructor
+        std::string axiom = "A"; // global variable representing the root node.
 
     protected:
         void setupTree(int width, int height, int numTree);
+        std::string generateString();
 
-    private:
-    // Any private member variables here
+    private: // private member variables here
+       // std::vector<std::string> m_finalString; // will update (Ashley)
 
 };
 
