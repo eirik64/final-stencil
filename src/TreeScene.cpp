6db5c9e694a1d433053598a9c7c855a212034c3e
Ashley Chung <ashleychung@Ashleys-MacBook-Pro-5.local>
2020-12-03 03:44:35 -0500

    Completed String Generator Method for L-System

--- src/TreeScene.cpp
+++ src/TreeScene.cpp
@@ -3,7 +3,14 @@
 #include <string>
 
 // Default constructor
-TreeScene::TreeScene() {}
+TreeScene::TreeScene() {
+    std::string firstGen = generateString(std::string("A")); // call generatestring on root node.
+    std::string secondGen = generateString(firstGen);
+    std::string thirdGen = generateString(secondGen); // produce strings over several generations to produce result strings..
+    std::string fourthGen = generateString(thirdGen);
+    // ... then use the resulting string to actually construct a tree.
+    setupTree(fourthGen, 500, 500, 1); // initialize tree
+}
 
 
 // Destructor
@@ -19,14 +26,44 @@ TreeScene::~TreeScene()
  *  Function: parses the string generated from the l-system rules
  *  and generates the tree.
  */
-void TreeScene::setupTree(int width, int height, int numTree) {}
-
-std::string TreeScene::generateString() {
-    std::string initialSentence = axiom; // starting with axiom.
-    std::string resultString;
-    for (int i=0; i < initialSentence.length(); ++i) {
-     //   std::string currentWord = initialSentence[i]
-        // still working on this (splitting the character, and then creating a new string based on each character
-        // will update this method. (Ashley)
-    }
+void TreeScene::setupTree(std::string inputStr, int width, int height, int numTree) { // 'numTree' parameter might not be used
+    // sets up the tree according to the final output string
+    // Haven't figured out how to keep track of position of branch, how to geometrically construct the tree, etc...
+    // also parameters are not finalized.. so feel free to propose a better structure!
+    //
 }
+
+/* Takes in character, replaces all the axiom letters to a new set of alphabetic and special character collection
+ * Input: an input string
+ * Output: output string changed according to rule
+ */
+std::string TreeScene::generateString(std::string chars) {
+    std::vector<char> finalVec;
+    std::vector<char> v(chars.begin(), chars.end()); // use iterator to create a vector of characters
+    for (int i=0; i < static_cast<int>(chars.size()); ++i) { // for every character in passed-in chars
+        char current = v[i]; // i-th element in our new vector
+        if (current == axiom) {
+            finalVec.push_back('A');
+            finalVec.push_back('+');
+            finalVec.push_back('-');
+            finalVec.push_back('[');
+            finalVec.push_back('B');
+            finalVec.push_back('+');
+            finalVec.push_back('A');
+            finalVec.push_back('-');
+            finalVec.push_back('[');
+            finalVec.push_back('B');
+            finalVec.push_back(']');
+            finalVec.push_back(']');
+            finalVec.push_back('-');
+            finalVec.push_back('[');
+            finalVec.push_back('A');
+            finalVec.push_back('B');
+            finalVec.push_back(']'); // replace with the rule
+        } else { // if it doesn't equal the axiom,
+            finalVec.push_back(current); // we don't need to replace with anything.
+        }
+    }
+    std::string outputString(finalVec.begin(), finalVec.end()); // convert to std string
+    return outputString;
+} // end of generateString() method
--- src/TreeScene.h
+++ src/TreeScene.h
@@ -2,25 +2,30 @@
 #define TREESCENE_H
 #include <iostream>
 #include <string>
+#include <memory.h>
+#include <QGLWidget>
 
 // rule 2
 struct rule {
     // variables: A, B, +, -, [, ]
     // axiom: A
     // rule: A → A+-[B+A-[B]]-[AB]
-    std::string startingVar = "A";
-    std::string convertedVar = "A+-[B+A-[B]]-[AB]"; // will update this. (Ashley)
+    char startingVar = 'A';
+    std::vector<char> convertedVar();
 };
+// store startingVar as a char and convertedVar as a vector of chars,
+//and just append to a final vector of chars as you replace your variables with the rules.
+// = std::make_unique<char>({'A'})
 
 class TreeScene {
     public: // public methods, global variables, constants here
         TreeScene(); // constructor
         ~TreeScene(); // destructor
-        std::string axiom = "A"; // global variable representing the root node.
+        char axiom = 'A'; // global variable representing the root node.
 
     protected:
-        void setupTree(int width, int height, int numTree);
-        std::string generateString();
+        void setupTree(std::string inputStr, int width, int height, int numTree);
+        std::string generateString(std::string chars);
 
     private: // private member variables here
        // std::vector<std::string> m_finalString; // will update (Ashley)
