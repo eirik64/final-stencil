da53c82216191cdab23d846a8913f4c22562406d
eirik64 <70987628+eirik64@users.noreply.github.com>
2020-12-03 11:32:39 -0500

    Some updates to generateString and treeSetUp

--- src/TreeScene.cpp
+++ src/TreeScene.cpp
@@ -31,7 +31,41 @@ void TreeScene::setupTree(std::string inputStr, int width, int height, int numTr
     // Haven't figured out how to keep track of position of branch, how to geometrically construct the tree, etc...
     // also parameters are not finalized.. so feel free to propose a better structure!
     //
+
 }
+
+// ERICK: I don't want to delete anything so I'll just kinda add stuff and we can sift through this afterwwards
+
+// This method will take in our generated string, the length of a branch that we generate,
+// our overall rotational angle and our recursive depth
+void TreeScene::setupTreeAlternative(std::string inputStr, float branchLength, float rotationAngle) {
+    std::vector<position> stack; // Pushes in positions at '[' and Pops them at ']'
+    // Current position as I go through the inputStr
+    position currentPos;
+    for (int i = 0; i < (int)inputStr.length(); i++) {
+        switch(inputStr[i]) {
+        case 'a':
+            // Do nothing
+            break;
+        case 'b':
+            // Draw line forward with the length of branchLength and update currentPos
+            break;
+        case '[':
+            // Pushes position into stack, no drawing
+            break;
+        case ']':
+            // Pops latest position and continues drawing from that position
+            break;
+        case '+':
+            // Updates current position, angle decreases by 22.5 to signify turning left
+            break;
+        case '-':
+            // Updates current position, angle increases by 22.5 to signify turning right
+            break;
+        }
+    }
+}
+
 
 /* Takes in character, replaces all the axiom letters to a new set of alphabetic and special character collection
  * Input: an input string
@@ -67,3 +101,80 @@ std::string TreeScene::generateString(std::string chars) {
     std::string outputString(finalVec.begin(), finalVec.end()); // convert to std string
     return outputString;
 } // end of generateString() method
+
+
+/*
+Please look over my code here. I'm supeer exhausteed by this point so this isn't really processing
+for me anymore. Feel free to change anything that may not make sense or scrap it. I'm going to sleep now ˙∆˚
+I'm assuming here that our axiom is just one character long... I hope
+Also, I included depth to represent the amount of times we went to walk
+through our string and update it based on our rules.
+I'm using the rules for the very last tree that I added in the docs. Subject to change**
+*/
+std::string TreeScene::generateStringAlt(std::string chars, int depth) {
+    std::vector<char> intermedVec;
+    std::vector<char> v(chars.begin(), chars.end());
+
+    for (int i = 0; i < depth; i++) {
+        for (int j = 0; j < (int) v.size(); j++) {
+            char current = v[i];
+            // B -> BB+[-B+B+B+B]-[B+B-B]
+            switch (current) {
+            case 'b':
+                intermedVec.push_back('b');
+                intermedVec.push_back('b');
+                intermedVec.push_back('+');
+                intermedVec.push_back('[');
+                intermedVec.push_back('-');
+                intermedVec.push_back('b');
+                intermedVec.push_back('+');
+                intermedVec.push_back('b');
+                intermedVec.push_back('+');
+                intermedVec.push_back('b');
+                intermedVec.push_back('+');
+                intermedVec.push_back('b');
+                intermedVec.push_back(']');
+                intermedVec.push_back('-');
+                intermedVec.push_back('[');
+                intermedVec.push_back('b');
+                intermedVec.push_back('+');
+                intermedVec.push_back('b');
+                intermedVec.push_back('-');
+                intermedVec.push_back('b');
+                intermedVec.push_back(']');
+                break;
+            // A -> B-[[A]+++A]B[+BA]-A
+            case 'a':
+                intermedVec.push_back('b');
+                intermedVec.push_back('-');
+                intermedVec.push_back('[');
+                intermedVec.push_back('[');
+                intermedVec.push_back('a');
+                intermedVec.push_back(']');
+                intermedVec.push_back('+');
+                intermedVec.push_back('+');
+                intermedVec.push_back('+');
+                intermedVec.push_back('a');
+                intermedVec.push_back(']');
+                intermedVec.push_back('b');
+                intermedVec.push_back('[');
+                intermedVec.push_back('+');
+                intermedVec.push_back('b');
+                intermedVec.push_back('a');
+                intermedVec.push_back(']');
+                intermedVec.push_back('-');
+                intermedVec.push_back('a');
+                break;
+            default:
+                intermedVec.push_back(current);
+            }
+        }
+        v = intermedVec;
+        intermedVec.clear();
+
+    }
+    std::string outputString(v.begin(), v.end()); // convert to std string
+    return outputString;
+}
+
+
--- src/TreeScene.h
+++ src/TreeScene.h
@@ -13,6 +13,13 @@ struct rule {
     char startingVar = 'A';
     std::vector<char> convertedVar();
 };
+
+// Stores the position and angle info needed when we encounter a '[' or ']'
+struct position {
+    float pos;
+    float angle;
+};
+
 // store startingVar as a char and convertedVar as a vector of chars,
 //and just append to a final vector of chars as you replace your variables with the rules.
 // = std::make_unique<char>({'A'})
@@ -21,11 +28,15 @@ class TreeScene {
     public: // public methods, global variables, constants here
         TreeScene(); // constructor
         ~TreeScene(); // destructor
+
+        // SUGGESTION: Potentially initialize as an std::string???
         char axiom = 'A'; // global variable representing the root node.
 
     protected:
         void setupTree(std::string inputStr, int width, int height, int numTree);
+        void setupTreeAlternative(std::string inputStr, float branchLength, float rotationAngle);
         std::string generateString(std::string chars);
+        std::string generateStringAlt(std::string chars, int depth);
 
     private: // private member variables here
        // std::vector<std::string> m_finalString; // will update (Ashley)
