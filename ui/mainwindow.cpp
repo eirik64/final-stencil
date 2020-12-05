e15346b0fe135ed11378014fd125be4620336782
Ashley Chung <ashleychung@Ashleys-MacBook-Pro-5.local>
2020-12-04 20:53:43 -0500

    update

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -12,14 +12,14 @@ MainWindow::MainWindow(QWidget *parent) :
     // TODO: 1. Copy over the orbit camera and settings files, and
     //       2. incorporate 3D canvas, camera, and settings object to the ui
 
-//    QGLFormat qglFormat;
-//    qglFormat.setVersion(4, 3);
-//    qglFormat.setProfile(QGLFormat::CoreProfile);
-//    qglFormat.setSampleBuffers(true);
-//    ui->setupUi(this);
-//    QGridLayout *gridLayout = new QGridLayout(ui->canvas3D);
-//    m_canvas3D = new SupportCanvas3D(qglFormat, this);
-//    gridLayout->addWidget(m_canvas3D, 0, 1);
+    QGLFormat qglFormat;
+    qglFormat.setVersion(4, 3);
+    qglFormat.setProfile(QGLFormat::CoreProfile);
+    qglFormat.setSampleBuffers(true);
+    ui->setupUi(this);
+    QGridLayout *gridLayout = new QGridLayout(ui->canvas3D);
+    m_canvas3D = new SupportCanvas3D(qglFormat, this);
+    gridLayout->addWidget(m_canvas3D, 0, 1);
     ui->setupUi(this);
 }
 
