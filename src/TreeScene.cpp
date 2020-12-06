dd93ae06b376bc362fa05e176c641d9ca24138ee
Ashley Chung <ashleychung@Ashleys-MacBook-Pro-5.local>
2020-12-06 14:29:08 -0500

    Cylinder problems with TreeScene

--- src/TreeScene.cpp
+++ src/TreeScene.cpp
@@ -2,10 +2,15 @@
 #include <iostream>
 #include <string>
 
+
 // Default constructor
-TreeScene::TreeScene() {
-    std::string resultString = generateStringAlt(axiom, 1); // start by depth 1.
-    setupTree(resultString, 500, 500, 1); // initialize tree using the result string from the recursive method.
+TreeScene::TreeScene()
+{
+//    std::string resultString = generateStringAlt(axiom, 1); // start by depth 1.
+//    m_cylinders = {};
+
+    //setupTreeAlternative(resultString, 1, 22.5); // initialize tree using the result string from the recursive method.
+    // branch length = 1 (using unit cylinder from shapes).
 }
 
 
@@ -22,123 +27,178 @@ TreeScene::~TreeScene()
  *  Function: parses the string generated from the l-system rules
  *  and generates the tree.
  */
-void TreeScene::setupTree(std::string inputStr, int width, int height, int numTree) { // 'numTree' parameter might not be used
-    // sets up the tree according to the final output string
-    // Haven't figured out how to keep track of position of branch, how to geometrically construct the tree, etc...
-    // also parameters are not finalized.. so feel free to propose a better structure!
-    //
+//void TreeScene::setupTree(std::string inputStr, int width, int height, int numTree) {
+//    // sets up the tree according to the final output string
 
+//}
+
+//std::vector<Cylinder> TreeScene::getPrimitiveList() {
+//    return m_cylinders;
+//}
+
+// VAO draw method
+void TreeScene::draw() {
+    std::make_unique<Cylinder>(1, 15)->draw();
+//    if (m_VAO) {
+//        m_VAO->bind();
+//        m_VAO->draw();
+//        m_VAO->unbind();
+//    }
 }
 
-// ERICK: I don't want to delete anything so I'll just kinda add stuff and we can sift through this afterwwards
+// build VAO
+void TreeScene::buildVAO() {
+//    const int numFloatsPerVertex = 6;
+//    const int numVertices = m_vertexData.size() / numFloatsPerVertex;
+
+//    std::vector<VBOAttribMarker> markers;
+//    markers.push_back(VBOAttribMarker(ShaderAttrib::POSITION, 3, 0));
+//    markers.push_back(VBOAttribMarker(ShaderAttrib::NORMAL, 3, 3*sizeof(float)));
+//    VBO vbo = VBO(m_vertexData.data(), m_vertexData.size(), markers);
+//    m_VAO = std::make_unique<VAO>(vbo, numVertices);
+}
 
 // This method will take in our generated string, the length of a branch that we generate,
 // our overall rotational angle and our recursive depth
+
+// we will modify m_cylinders, push each cylinder to the vector,
 void TreeScene::setupTreeAlternative(std::string inputStr, float branchLength, float rotationAngle) {
     std::vector<position> stack; // Pushes in positions at '[' and Pops them at ']'
-    // Current position as I go through the inputStr
-    position currentPos;
+
     for (int i = 0; i < (int)inputStr.length(); i++) {
         switch(inputStr[i]) {
-        case 'a':
-            // Do nothing
-            break;
-        case 'b':
-            // Draw line forward with the length of branchLength and update currentPos
-            break;
-        case '[':
-            // Pushes position into stack, no drawing
-            break;
-        case ']':
-            // Pops latest position and continues drawing from that position
-            break;
-        case '+':
-            // Updates current position, angle decreases by 22.5 to signify turning left
-            break;
-        case '-':
-            // Updates current position, angle increases by 22.5 to signify turning right
-            break;
+            case 'a': {
+                // do nothing
+                break;
+            }
+            case 'b': { // draw a line at curr angle.
+
+                const Cylinder cylinder = Cylinder(1,15); // initialize new cylinder
+//                std::unique_ptr<Cylinder> cylinder = std::make_unique<Cylinder>(1, 15);
+                Branch newBranch;
+                newBranch.currBranch = cylinder;
+                newBranch.transformation = glm::mat4x4(1)*glm::translate(glm::vec3(0,1,0)); // translate upward by height of the cylinder, 1
+                m_cylinders.push_back(newBranch); // push it into
+
+
+//                if (m_cylinders.size() == 0) { // when it's the first branch to be added to the list of cylinders
+//                 //   newBranch.currBranch = std::make_unique<Cylinder>(1, 15);
+//                    newBranch.transformation = glm::mat4x4(1); // Identity matrix
+//                    newBranch.position = glm::vec3(0,0,0); // have to set the initial position.
+//                } else {
+//              //      newBranch.currBranch = std::make_unique<Cylinder>(1, 15);
+//                    newBranch.transformation = glm::mat4x4(1); // Identity matrix, equiv. to the step of "doing nothing"
+//                    newBranch.position = m_cylinders[m_cylinders.size()-1].position + glm::vec3(0,1,0); // just increase in y-direction according to cylinder height (1, in this case)
+//                }
+//                m_cylinders.push_back(newBranch); // push back the new branch
+//                // Draw line forward with the length of branchLength and update currentPos
+                break;
+            }
+            case '[': {
+                // Pushes position into stack, no drawing
+
+                break;
+            }
+            case ']': {
+                // Pops latest position and continues drawing from that position
+                break;
+            }
+//            case '+': {
+//                // Updates current position, angle decreases by 22.5 to signify turning left
+//            Branch newBranch; // param1 = 1, param2 = 15
+//            if (m_cylinders.size() == 0) { // when it's the first branch to be added to the list of cylinders
+////                newBranch.currBranch = std::make_unique<Cylinder>(1, 15);
+////                newBranch.transformation = glm::rotate(glm::mat4x4(1), rotationAngle); // rotation 22.5 degrees
+//               // glm::rotate(compositeMat, transform->angle, transform->rotate)
+//                newBranch.position = glm::vec3(0,0,0); // have to set the initial position.
+//            } else {
+
+////                newBranch.currBranch = std::make_unique<Cylinder>(1, 15);
+////                newBranch.transformation = glm::rotate(glm::mat4x4(1), rotationAngle); // Identity matrix, equiv. to the step of "doing nothing"
+////                newBranch.position = m_cylinders[m_cylinders.size()-1].position + glm::vec3(0,1,0); // just increase in y-direction according to cylinder height (1, in this case)
+//            }
+//            m_cylinders.push_back(newBranch); // push back the new branch
+//                break;
+//            }
+//            case '-': {
+//                // Updates current position, angle increases by 22.5 to signify turning right
+//                break;
+//            }
         }
     }
 }
-
-// Ashley : Deleted my version of generateString but I am keeping it separately in another file just in case.
 
 /*
-˙∆˚ I like this face
-I looked over your code. (Ashley)
-Axiom is indeed one character long!
-Depth is a good idea, since we need to indicate how many times we want to recursively call this method.
-Using the rules for the very last tree in the google doc
 */
 std::string TreeScene::generateStringAlt(std::string chars, int depth) {
-    std::vector<char> intermedVec;
-    std::vector<char> v(chars.begin(), chars.end());
+//    std::vector<char> intermedVec;
+//    std::vector<char> v(chars.begin(), chars.end());
 
-    for (int i = 0; i < depth; i++) {
-        for (int j = 0; j < (int) v.size(); j++) {
-            char current = v[i];
-            // B -> BB+[-B+B+B+B]-[B+B-B]
-            switch (current) {
-            case 'b':
-                intermedVec.push_back('b');
-                intermedVec.push_back('b');
-                intermedVec.push_back('+');
-                intermedVec.push_back('[');
-                intermedVec.push_back('-');
-                intermedVec.push_back('b');
-                intermedVec.push_back('+');
-                intermedVec.push_back('b');
-                intermedVec.push_back('+');
-                intermedVec.push_back('b');
-                intermedVec.push_back('+');
-                intermedVec.push_back('b');
-                intermedVec.push_back(']');
-                intermedVec.push_back('-');
-                intermedVec.push_back('[');
-                intermedVec.push_back('b');
-                intermedVec.push_back('+');
-                intermedVec.push_back('b');
-                intermedVec.push_back('-');
-                intermedVec.push_back('b');
-                intermedVec.push_back(']'); // This looks correct, based on our rules!
-                break;
-            // A -> B-[[A]+++A]B[+BA]-A
-            case 'a':
-                intermedVec.push_back('b');
-                intermedVec.push_back('-');
-                intermedVec.push_back('[');
-                intermedVec.push_back('[');
-                intermedVec.push_back('a');
-                intermedVec.push_back(']');
-                intermedVec.push_back('+');
-                intermedVec.push_back('+');
-                intermedVec.push_back('+');
-                intermedVec.push_back('a');
-                intermedVec.push_back(']');
-                intermedVec.push_back('b');
-                intermedVec.push_back('[');
-                intermedVec.push_back('+');
-                intermedVec.push_back('b');
-                intermedVec.push_back('a');
-                intermedVec.push_back(']');
-                intermedVec.push_back('-');
-                intermedVec.push_back('a'); // This looks correct also.
-                break;
-            default:
-                intermedVec.push_back(current);
-            }
-        }
-        v = intermedVec;
-        intermedVec.clear();
-    } // end of double for loop
+//    for (int i = 0; i < depth; i++) {
+//        for (int j = 0; j < (int) v.size(); j++) {
+//            char current = v[i];
+//            // B -> BB+[-B+B+B+B]-[B+B-B]
+//            switch (current) {
+//            case 'b':
+//                intermedVec.push_back('b');
+//                intermedVec.push_back('b');
+//                intermedVec.push_back('+');
+//                intermedVec.push_back('[');
+//                intermedVec.push_back('-');
+//                intermedVec.push_back('b');
+//                intermedVec.push_back('+');
+//                intermedVec.push_back('b');
+//                intermedVec.push_back('+');
+//                intermedVec.push_back('b');
+//                intermedVec.push_back('+');
+//                intermedVec.push_back('b');
+//                intermedVec.push_back(']');
+//                intermedVec.push_back('-');
+//                intermedVec.push_back('[');
+//                intermedVec.push_back('b');
+//                intermedVec.push_back('+');
+//                intermedVec.push_back('b');
+//                intermedVec.push_back('-');
+//                intermedVec.push_back('b');
+//                intermedVec.push_back(']');
+//                break;
+//            // A -> B-[[A]+++A]B[+BA]-A
+//            case 'a':
+//                intermedVec.push_back('b');
+//                intermedVec.push_back('-');
+//                intermedVec.push_back('[');
+//                intermedVec.push_back('[');
+//                intermedVec.push_back('a');
+//                intermedVec.push_back(']');
+//                intermedVec.push_back('+');
+//                intermedVec.push_back('+');
+//                intermedVec.push_back('+');
+//                intermedVec.push_back('a');
+//                intermedVec.push_back(']');
+//                intermedVec.push_back('b');
+//                intermedVec.push_back('[');
+//                intermedVec.push_back('+');
+//                intermedVec.push_back('b');
+//                intermedVec.push_back('a');
+//                intermedVec.push_back(']');
+//                intermedVec.push_back('-');
+//                intermedVec.push_back('a');
+//                break;
+//            default:
+//                intermedVec.push_back(current);
+//            }
+//        }
+//        v = intermedVec;
+//        intermedVec.clear();
+//    } // end of double for loop
 
-    std::string outputString(v.begin(), v.end()); // convert to std string
-    if (depth != maximumDepth) {
-        std::string resultString = generateStringAlt(outputString, depth + 1); // make a recursive call
-        std::string outputString(resultString.begin(), resultString.end());
-       // std::string outputString(v.begin(), v.end()); // again, convert to std::string
-    }
-    std::cout<<"output string: "<<outputString<<std::endl;
-    return outputString;
+//    std::string outputString(v.begin(), v.end()); // convert to std string
+//    if (depth != maximumDepth) { // if not maximum depth,
+//        std::string resultString = generateStringAlt(outputString, depth + 1); // make a recursive call
+//        std::string outputString(resultString.begin(), resultString.end());
+//       // std::string outputString(v.begin(), v.end()); // again, convert to std::string
+//    }
+//    std::cout<<"output string: "<<outputString<<std::endl;
+//    return outputString;
 }
+
--- src/TreeScene.h
+++ src/TreeScene.h
@@ -1,40 +1,51 @@
 #ifndef TREESCENE_H
 #define TREESCENE_H
+#include "shapes/Cylinder.h"
 #include <iostream>
 #include <string>
 #include <memory.h>
 #include <QGLWidget>
+#include "glm/gtx/transform.hpp"
 
-// struct representing rule
-// store startingVar as a char and convertedVar as a vector of chars,
-// and just append to a final vector of chars as you replace your variables with the rules.
-struct rule {
-    char startingVar = 'A';
-    std::vector<char> convertedVar();
-};
+
+namespace CS123 { namespace GL {
+class VAO;
+}}
 
 // Stores the position and angle info needed when we encounter a '[' or ']'
 struct position {
-    float pos;
+    glm::mat4x4 accMat;
     float angle;
+};
+
+// struct representing each cylinder for tree-building
+struct Branch {
+    Cylinder currBranch;
+    glm::mat4x4 transformation; // transformation matrix for that particular branch. (accumulated view, translate, projection matrices)
+    glm::vec3 position;
 };
 
 class TreeScene {
     public: // public methods, global variables, constants here
         TreeScene(); // constructor
         ~TreeScene(); // destructor
+        void draw();
 
         std::string axiom = "A"; // global variable representing the root node.(initialize as std::string)
-        int maximumDepth = 5; // maximum depth global variable.
+        int maximumDepth = 5; // maximum recursion depth - global variable.
+        std::vector<Cylinder> getPrimitiveList(); // returns the member variable (a vector of cylinders)
 
     protected:
         void setupTree(std::string inputStr, int width, int height, int numTree);
         void setupTreeAlternative(std::string inputStr, float branchLength, float rotationAngle);
-        std::string generateString(std::string chars);
         std::string generateStringAlt(std::string chars, int depth);
 
     private: // private member variables here
+        void buildVAO();
+        std::vector<GLfloat> m_vertexData;
+//        std::unique_ptr<CS123::GL::VAO> m_VAO;
        // std::vector<std::string> m_finalString; // will update (Ashley)
+        std::vector<Branch> m_cylinders; // vector that stores primitives and their transformation (composite matrix)
 
 };
 
