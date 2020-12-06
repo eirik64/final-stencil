d93b74541d11072b8211cf5e2996ea1c96dac614
eirik64 <70987628+eirik64@users.noreply.github.com>
2020-12-06 13:53:34 -0500

    Camera Updates

--- Camera/Camera.cpp
+++ Camera/Camera.cpp
@@ -6,29 +6,36 @@
 
 #include "Camera.h"
 #include "Settings.h"
-Camera::Camera() :
-    m_aspectRatio(1),
-    m_angleX(0),
-    m_angleY(0),
-    m_zoomZ(-5),
-    m_eye(glm::vec4(-2.f, 0.f, 0.f, 1.f))
-
+Camera::Camera()
 {
-
+    m_aspectRatio = 1.f;
+    m_near = 1.f;
+    m_far = 30.f;
+    m_thetaH = glm::radians(60.f);
+    m_thetaW = 2 * glm::atan(m_aspectRatio * glm::tan(m_thetaH/2));
+    m_eye = glm::vec4(0.f, 0.f, 2.f, 1.f);
+    m_up = glm::vec4(0.f, 1.f, 0.f, 0.f);
+    m_w = glm::normalize(glm::vec4(2.f, 2.f, 2.f, 0));
+    m_v = glm::normalize(m_up - glm::dot(m_up,m_w)*m_w);
+    m_u = glm::normalize(glm::vec4(glm::cross(glm::vec3(m_v), glm::vec3(m_w)), 0));
+    updateProjectionMatrix();
+    updateViewMatrix();
 }
 
 
-void Camera::setAspectRatio(float aspectRatio) {
-    m_aspectRatio = aspectRatio;
+void Camera::setAspectRatio(float a)
+{
+    m_aspectRatio = a;
+    m_thetaW = 2 * glm::atan(m_aspectRatio * glm::tan(m_thetaH/2));
     updateProjectionMatrix();
 }
 
 glm::mat4x4 Camera::getProjectionMatrix() const {
-    return m_projectionMatrix;
+    return m_perspectiveTransformation*m_scaleMatrix;
 }
 
 glm::mat4x4 Camera::getViewMatrix() const {
-    return m_viewMatrix;
+    return m_rotationMatrix*m_translationMatrix;
 }
 
 glm::mat4x4 Camera::getScaleMatrix() const {
@@ -36,54 +43,142 @@ glm::mat4x4 Camera::getScaleMatrix() const {
 }
 
 glm::mat4x4 Camera::getPerspectiveMatrix() const {
-    throw 0; // not implemented
+    return m_perspectiveTransformation;
 }
 
 glm::vec4 Camera::getPosition() const {
     return m_eye;
 }
 
+glm::vec4 Camera::getLook() const {
+    return -m_w;
+}
+
+glm::vec4 Camera::getUp() const {
+    return m_up;
+}
+
+glm::vec4 Camera::getU() const {
+    return m_u;
+}
+
+glm::vec4 Camera::getV() const {
+    return m_v;
+}
+
+glm::vec4 Camera::getW() const {
+    return m_w;
+}
+
+float Camera::getAspectRatio() const {
+    return m_aspectRatio;
+}
+
+float Camera::getHeightAngle() const {
+    return m_thetaH;
+}
+
 void Camera::updatePosition(float x, float y, float z) {
-    m_angleX += x - m_eye.x;
-    m_angleY += y - m_eye.y;
-    if (m_angleX < -90.f) m_angleX = -90.f;
-    if (m_angleX > 90.f) m_angleX = 90.f;
-
     m_eye = glm::vec4(x, y, z, 1.f);
-
     updateViewMatrix();
 }
 
 
-void Camera::mouseScrolled(int delta) {
-    // Use an exponential factor so the zoom increments are small when we are
-    // close to the object and large when we are far away from the object
-    m_zoomZ *= powf(0.999f, delta);
-
+void Camera::orientLook(const glm::vec4 &eye, const glm::vec4 &look, const glm::vec4 &up) {
+    m_eye = eye;
+    m_up = glm::normalize(up);
+    m_w = -glm::normalize(look);
+    glm::vec4 vCalc = m_up - glm::dot(m_up,m_w)*m_w;
+    m_v = glm::normalize(vCalc);
+    m_u = glm::normalize(glm::vec4(glm::cross(glm::vec3(m_v), glm::vec3(m_w)), 0));
     updateViewMatrix();
-}
-
-void Camera::updateMatrices() {
     updateProjectionMatrix();
+}
+
+void Camera::setHeightAngle(float h) {
+    m_thetaH = glm::radians(h);
+    m_thetaW = 2 * glm::atan(m_aspectRatio * glm::tan(m_thetaH/2));
+    updateProjectionMatrix();
+}
+
+/*
+ * Probably won't need to implement this stuff
+ *
+void Camera::translate(const glm::vec4 &v) {
+    m_eye += v;
     updateViewMatrix();
+
+}
+
+void Camera::rotateU(float degrees) {
+    // @TODO: [] Fill this in...
+    glm::vec4 vNot = m_v;
+    glm::vec4 wNot = m_w;
+    m_v = wNot * glm::sin(glm::radians(degrees)) + vNot * glm::cos(glm::radians(degrees));
+    m_w = wNot * glm::cos(glm::radians(degrees)) - vNot * glm::sin(glm::radians(degrees));
+    updateViewMatrix();
+
+}
+
+void Camera::rotateV(float degrees) {
+    // @TODO: [] Fill this in...
+    glm::vec4 uNot = m_u;
+    glm::vec4 wNot = m_w;
+    m_u= uNot * glm::cos(glm::radians(degrees)) - wNot * glm::sin(glm::radians(degrees));
+    m_w = uNot * glm::sin(glm::radians(degrees)) + wNot * glm::cos(glm::radians(degrees));
+    updateViewMatrix();
+
+}
+
+void Camera::rotateW(float degrees) {
+    // @TODO: [] Fill this in...
+    glm::vec4 uNot = m_u;
+    glm::vec4 vNot = m_v;
+    m_u = (uNot * glm::cos(-glm::radians(degrees))) - (vNot * glm::sin(-glm::radians(degrees)));
+    m_v = (uNot * glm::sin(-glm::radians(degrees))) + (vNot * glm::cos(-glm::radians(degrees)));
+    updateViewMatrix();
+
+}
+*/
+void Camera::setClip(float nearPlane, float farPlane) {
+    m_near = nearPlane;
+    m_far = farPlane;
+    updateProjectionMatrix();
 }
 
 void Camera::updateProjectionMatrix() {
-    // Make sure glm gets a far value that is greater than the near value.
-    // Thanks Windows for making far a keyword!
-    float farPlane = std::max(settings.cameraFar, settings.cameraNear + 100.f * FLT_EPSILON);
-    float h = farPlane * glm::tan(glm::radians(settings.cameraFov * 0.5f));
-    float w = m_aspectRatio * h;
+    updateScaleMatrix();
+    updatePerspectiveMatrix();
 
-    m_scaleMatrix = glm::scale(glm::vec3(1.f / w, 1.f / h, 1.f / farPlane));
-    m_projectionMatrix = glm::perspective(
-            glm::radians(settings.cameraFov), m_aspectRatio, settings.cameraNear, farPlane) * 0.02f;
 }
+void Camera::updatePerspectiveMatrix() {
+    m_perspectiveTransformation = glm::transpose(glm::mat4(1, 0, 0, 0,
+                                                           0, 1, 0, 0,
+                                                           0, 0, -1/((-m_near/m_far) + 1), (-m_near/m_far)/((-m_near/m_far) + 1),
+                                                           0, 0, -1, 0));
 
-void Camera::updateViewMatrix() {
-    m_viewMatrix =
-            glm::translate(glm::vec3(0.f, 0.f, m_zoomZ)) *
-            glm::rotate(glm::radians(m_angleY), glm::vec3(0.f, 1.f, 0.f)) *
-            glm::rotate(glm::radians(m_angleX), glm::vec3(1.f, 0.f, 0.f));
 }
+void Camera::updateScaleMatrix() {
+    m_scaleMatrix = glm::transpose(glm::mat4((1/(glm::tan(m_thetaW/2))/m_far), 0, 0, 0,
+                                             0, (1/(glm::tan(m_thetaH/2))/m_far), 0, 0,
+                                             0, 0, 1/m_far, 0,
+                                             0, 0, 0, 1));
 
+}
+void Camera::updateViewMatrix(){
+    updateTranslationMatrix();
+    updateRotationMatrix();
+}
+void Camera::updateRotationMatrix(){
+    m_rotationMatrix = glm::transpose(glm::mat4(m_u.x, m_u.y, m_u.z, 0,
+                                                m_v.x, m_v.y, m_v.z, 0,
+                                                m_w.x, m_w.y, m_w.z, 0,
+                                                0, 0, 0, 1));
+
+}
+void Camera::updateTranslationMatrix(){
+    m_translationMatrix = glm::transpose(glm::mat4(1, 0, 0, -m_eye.x,
+                                                   0, 1, 0, -m_eye.y,
+                                                   0, 0, 1, -m_eye.z,
+                                                   0, 0, 0, 1));
+}
--- Camera/Camera.h
+++ Camera/Camera.h
@@ -27,17 +27,59 @@ public:
     // settings.
     virtual glm::mat4x4 getPerspectiveMatrix() const;
 
-    // Returns the current position of the camera
+    // Returns the current position of the camera.
     glm::vec4 getPosition() const;
 
-    virtual void mouseScrolled(int delta);
-    void updateMatrices();
-    void updatePosition(float x, float y, float z);
+    // Returns the current 'look' vector for this camera.
+    glm::vec4 getLook() const;
 
-private:
+    // Returns the 'u' vector for this camera
+    glm::vec4 getU() const;
+
+    // Returns the 'v' vector for this camera
+    glm::vec4 getV() const;
+
+    // Returns the 'w' vector for this camera
+    glm::vec4 getW() const;
+
+    // Returns the current 'up' vector for this camera (the 'V' vector).
+    glm::vec4 getUp() const;
+
+    // Returns the currently set aspect ratio.
+    float getAspectRatio() const;
+
+    // Returns the currently set height angle.
+    float getHeightAngle() const;
+
+    // Move this camera to a new eye position, and orient the camera's axes given look and up
+    // vectors.
+    void orientLook(const glm::vec4 &eye, const glm::vec4 &look, const glm::vec4 &up);
+
+    // Sets the height angle of this camera.
+    void setHeightAngle(float h);
+
+    // Translates the camera along a given vector.
+    void translate(const glm::vec4 &v);
+
+    // Rotates the camera about the U axis by a specified number of degrees.
+    void rotateU(float degrees);
+
+    // Rotates the camera about the V axis by a specified number of degrees.
+    void rotateV(float degrees);
+
+    // Rotates the camera about the W axis by a specified number of degrees.
+    void rotateW(float degrees);
+
+    // Sets the near and far clip planes for this camera.
+    void setClip(float nearPlane, float farPlane);
+
     void updateProjectionMatrix();
+    void updatePerspectiveMatrix();
+    void updateScaleMatrix();
     void updateViewMatrix();
-
+    void updateRotationMatrix();
+    void updateTranslationMatrix();
+    void updatePosition(float x, float y, float z);
 
     float m_aspectRatio;
     float m_near;
